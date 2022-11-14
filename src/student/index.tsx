6a65ffc62f82a2a2ae7ccf2d4981a927f8fce5b6
Kirill Penkin <kirill.penkin.doubleverify.com>
2022-11-14 12:41:27 +0400

    entry points for both applications

---
+++ src/student/index.tsx
@@ -0,0 +1,9 @@
+import { HOC } from "../common/types/utility";
+
+const StudentEntryPoint: HOC = () => (
+  <>
+    <div>Student</div>
+  </>
+);
+
+export default StudentEntryPoint;
---
+++ src/teacher/index.tsx
@@ -0,0 +1,9 @@
+import { HOC } from "../common/types/utility";
+
+const TeacherEntryPoint: HOC = () => (
+  <>
+    <div>Teacher</div>
+  </>
+);
+
+export default TeacherEntryPoint;
