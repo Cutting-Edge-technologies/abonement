98e26661f9ff7a70d8bf3a32d12177989d7d49e4
Kirill Penkin <kirill.penkin.doubleverify.com>
2022-11-17 20:19:40 +0400

    add to root reducer

--- src/teacher/store/rootReducer.ts
+++ src/teacher/store/rootReducer.ts
@@ -1,10 +1,19 @@
 import { combineReducers } from "redux";
 import { errorSlice } from "../../common/store/errorSlice";
 import { loadingSlice } from "../../common/store/loadingSlice";
+import { teacherDomainViewSlice } from "./domainView";
 import { exampleSlice } from "./example";
+import * as resourceSlices from './teacherExternalResources';
+
+const { externalLessonsSlice, externalSubjectsSlice, externalTeachersSlice, externalUsersSlice } = resourceSlices;
 
 export const teacherRootReducer = combineReducers({
   loading: loadingSlice.reducer,
   error: errorSlice.reducer,
   example: exampleSlice.reducer,
+  teacherDomain: teacherDomainViewSlice.reducer,
+  externalLessons: externalLessonsSlice.reducer,
+  externalSubjects: externalSubjectsSlice.reducer,
+  externalTeachers: externalTeachersSlice.reducer,
+  externalUsers: externalUsersSlice.reducer,
 });
