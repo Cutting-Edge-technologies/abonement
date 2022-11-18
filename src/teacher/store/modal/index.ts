e0a0dfdba88c21d7f4c8c5388ff40b81c6eceb80
Kirill Penkin <kirill.penkin.doubleverify.com>
2022-11-18 13:53:43 +0400

    teacher modal state

---
+++ src/teacher/store/modal/index.ts
@@ -0,0 +1,66 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export enum TecherCalendarRepresentationType {
+  schedule = 'schedule',
+  calendarWeek = 'calendarWeek',
+}
+
+export enum TeacherView {
+  schedule = 'schedule',
+  subjectList = 'subjectList',
+  subjectPage = 'subjectPage',
+}
+
+export enum SubjectListView {
+  subjects = 'subjects',
+  abonements = 'abonements',
+}
+
+export enum ModalMode {
+  fillHeight = 'fillHeight',
+  halfHeight = 'halfHeight',
+}
+
+export enum LessonView {
+  accepted = 'accepted',
+  pending = 'pending',
+  declained = 'declained',
+}
+
+interface ITeacherModalState {
+  techerCalendarRepresentationType: TecherCalendarRepresentationType;
+  teacherView: TeacherView;
+  subjectListView: SubjectListView;
+
+  isAbonementModalOpen: boolean;
+
+  isNewRuleModalOpen: boolean;
+  ruleModalMode:ModalMode;
+
+  lessonView: LessonView;
+
+  isLessonAddParticipantModalOpen: boolean;
+}
+
+const teacherModalInitialState: ITeacherModalState = {
+  techerCalendarRepresentationType: TecherCalendarRepresentationType.schedule,
+  teacherView: TeacherView.schedule,
+  subjectListView: SubjectListView.subjects,
+  lessonView: LessonView.accepted,
+  isAbonementModalOpen: false,
+  isLessonAddParticipantModalOpen: false,
+  isNewRuleModalOpen: false,
+  ruleModalMode: ModalMode.halfHeight,
+};
+
+export const teacherModalSlice = createSlice({
+  name: 'teacherModal',
+  initialState: teacherModalInitialState,
+  reducers: {
+    setModalState: (state, { payload: stateDiff }: PayloadAction<Partial<ITeacherModalState>>) => ({
+      ...state,
+      ...stateDiff,
+    }),
+    resetModalState: () => ({...teacherModalInitialState}),
+  },
+});
