b3a721a0f6dba7f846d786659d527b64847f52c3
Kirill Penkin <kirill.penkin.doubleverify.com>
2022-11-21 22:36:22 +0400

    typed use selector hook

--- src/teacher/store/index.ts
+++ src/teacher/store/index.ts
@@ -1,8 +1,11 @@
 import { makeStoreCreator } from "../../common/store/utils";
 import { teacherRootReducer } from "./rootReducer";
 import { teacherRootSaga } from "../commands";
+import { TypedUseSelectorHook, useSelector } from "react-redux";
 
 export type rootTeacherState = ReturnType<typeof teacherRootReducer>;
+
+export const useTeacherSelector = useSelector as TypedUseSelectorHook<rootTeacherState>;
 
 export const teacherStoreCreator = makeStoreCreator(teacherRootReducer, teacherRootSaga);
 
