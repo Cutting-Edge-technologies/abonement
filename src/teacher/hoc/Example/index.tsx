491edbf268670ccbafdac606eca229c3ab216db2
Kirill Penkin <kirill.penkin.doubleverify.com>
2022-11-21 22:36:43 +0400

    add some dummy functionality to the example hoc

--- src/teacher/hoc/Example/index.tsx
+++ src/teacher/hoc/Example/index.tsx
@@ -1,22 +1,35 @@
 import { useDispatch } from "react-redux";
 import { HOC } from "../../../common/types/utility";
-import { exampleCommand, exampleApiCommand } from "../../commands/example/exampleCommands";
+import { exampleCommand, exampleApiCommand, exampleResetCommand } from "../../commands/example/exampleCommands";
+import { useTeacherSelector } from "../../store";
 
 export const ExampleTeacher: HOC = () => {
   const dispatch = useDispatch();
+  const message = useTeacherSelector((state) => state.example.message);
+  const count = useTeacherSelector((state) => state.example.count);
   return (
     <>
       <div>Teacher</div>
-      <button
-        onClick={() => dispatch(exampleCommand.action('Test message'))}
-      >
-        Call string command
-      </button>
-      <button
-        onClick={() => dispatch(exampleApiCommand.action(42))}
-      >
-        Call number command
-      </button>
+      <div className="message">{`The message is ${message}`}</div>
+      <div className="count">{`The count is ${count}`}</div>
+      <div className="controlls" style={{display: 'flex', flexDirection: 'column', width: '60%'}}>
+        <button
+          onClick={() => dispatch(exampleCommand.action('Test message'))}
+        >
+          Call string command
+        </button>
+        <button
+          onClick={() => dispatch(exampleApiCommand.action(42))}
+        >
+          Call number command
+        </button>
+        <button
+          onClick={() => dispatch(exampleResetCommand.action())}
+        >
+          Call reset command
+        </button>
+      </div>
+      
     </>
   )
 }
