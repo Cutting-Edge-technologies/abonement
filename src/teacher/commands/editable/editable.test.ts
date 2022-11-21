2ba85950fa735b6ace37f86ece7fa815e3590f88
UrUrZa <ishvarIndrovich@mail.ru>
2022-11-21 17:34:41 +0400

    test changeAbonementPrice

--- src/teacher/commands/editable/editable.test.ts
+++ src/teacher/commands/editable/editable.test.ts
@@ -1,6 +1,5 @@
-import {changeAbonementLimitLessons} from "./editable";
+import {changeAbonementLimitLessons, changeAbonementPrice} from "./editable";
 import {teacherStoreCreator} from "../../store"
-import { Selector } from "@reduxjs/toolkit";
 import { TeacherSelector } from "../../../common/types/utility";
 import { IAbonementOffer } from "../../../common/types/domain";
 import { editingFields, IEditingResourceState } from "../../../common/store/editingEntitySlice";
@@ -9,8 +8,11 @@ const selectAbonementOffer: TeacherSelector<IEditingResourceState<IAbonementOffe
 const selectAbonementOfferData: TeacherSelector<IAbonementOffer> = (state) => selectAbonementOffer(state).data;
 const selectAbonementOfferEditingFields: TeacherSelector<editingFields> = (state) => selectAbonementOffer(state).fieldsEditing;
 const selectAbonementOfferLimitLessons: TeacherSelector<number> = (state) => selectAbonementOfferData(state).limitLessons; 
+const selectAbonementOfferPrice: TeacherSelector<number> = (state) => selectAbonementOfferData(state).price; 
 
-describe('Teacher Editable Commands', () => {
+
+describe('Teacher Editable AbonementsOffer Commands', () => {
+
   test('change Abonement Limit Lessons test', async () => {
     const teacherStore = teacherStoreCreator();
     const initialState = teacherStore.getState();
@@ -19,6 +21,16 @@ describe('Teacher Editable Commands', () => {
     await teacherStore.asyncDispatch(changeAbonementLimitLessons.action(newLimitLessons));
     const changedState = teacherStore.getState();
     expect(selectAbonementOfferLimitLessons(changedState)).toBe(newLimitLessons);
+  });
+
+  test('change Abonement Price test', async () => {
+    const teacherStore = teacherStoreCreator();
+    const initialState = teacherStore.getState();
+    expect(selectAbonementOfferPrice(initialState)).toBe(0);
+    const newPrice = 1500;
+    await teacherStore.asyncDispatch(changeAbonementPrice.action(newPrice));
+    const changedState = teacherStore.getState();
+    expect(selectAbonementOfferPrice(changedState)).toBe(newPrice);
   });
 });
 
