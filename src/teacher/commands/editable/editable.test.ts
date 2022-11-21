e73fa7ee49820d64bb1e1881709ba940e311f661
UrUrZa <ishvarIndrovich@mail.ru>
2022-11-21 17:10:10 +0400

    test change abonement limit lessons

---
+++ src/teacher/commands/editable/editable.test.ts
@@ -0,0 +1,24 @@
+import {changeAbonementLimitLessons} from "./editable";
+import {teacherStoreCreator} from "../../store"
+import { Selector } from "@reduxjs/toolkit";
+import { TeacherSelector } from "../../../common/types/utility";
+import { IAbonementOffer } from "../../../common/types/domain";
+import { editingFields, IEditingResourceState } from "../../../common/store/editingEntitySlice";
+
+const selectAbonementOffer: TeacherSelector<IEditingResourceState<IAbonementOffer>> = (state) => state.editableAbonementOffer;
+const selectAbonementOfferData: TeacherSelector<IAbonementOffer> = (state) => selectAbonementOffer(state).data;
+const selectAbonementOfferEditingFields: TeacherSelector<editingFields> = (state) => selectAbonementOffer(state).fieldsEditing;
+const selectAbonementOfferLimitLessons: TeacherSelector<number> = (state) => selectAbonementOfferData(state).limitLessons; 
+
+describe('Teacher Editable Commands', () => {
+  test('change Abonement Limit Lessons test', async () => {
+    const teacherStore = teacherStoreCreator();
+    const initialState = teacherStore.getState();
+    expect(selectAbonementOfferLimitLessons(initialState)).toBe(0);
+    const newLimitLessons = 15;
+    await teacherStore.asyncDispatch(changeAbonementLimitLessons.action(newLimitLessons));
+    const changedState = teacherStore.getState();
+    expect(selectAbonementOfferLimitLessons(changedState)).toBe(newLimitLessons);
+  });
+});
+
