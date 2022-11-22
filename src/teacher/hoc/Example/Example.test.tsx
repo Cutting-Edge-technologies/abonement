3893b0445a92304300519a82636767c6090224a1
Kirill Penkin <kirill.penkin.doubleverify.com>
2022-11-22 14:12:29 +0400

    add to test

--- src/teacher/hoc/Example/Example.test.tsx
+++ src/teacher/hoc/Example/Example.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 import { ExampleTeacher } from '.';
 import { HocDecorator } from '../TeacherStateDecorator';
 
@@ -8,6 +9,24 @@ describe('Test Example HOC', () => {
     const { container } = await render(HocStoryToRender);
     const messageElement = container.querySelector('.message');
     console.log(messageElement?.innerHTML);
+
+    const buttons = container.querySelectorAll('button');
+    const buttonSet = buttons[1];
+    const buttonsReset = buttons[2];
+
+    await act(async () => {
+      buttonSet.click();
+      await new Promise(r => setTimeout(r, 100));
+      console.log('After set');
+      console.log(messageElement?.innerHTML);
+    });
+
+    await act(async () => {
+      buttonsReset.click();
+      await new Promise(r => setTimeout(r, 100));
+      console.log('After reset');
+      console.log(messageElement?.innerHTML);
+    })
 
     expect(true).toBeTruthy();
   })
