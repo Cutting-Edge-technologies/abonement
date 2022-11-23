1342402fd4d4866e585bd0e0da39f713bc106ea0
UrUrZa <IshvarIndrovich@mail.ru>
2022-11-23 13:48:06 +0400

    test subject and teacher Decription ShowMore

--- src/teacher/commands/novigation/novigation.test.ts
+++ src/teacher/commands/novigation/novigation.test.ts
@@ -3,12 +3,17 @@ import {TeacherSelector} from "../../../common/types/utility";
 import { ModalMode, TecherCalendarRepresentationType } from "../../store/modal";
 import {
   setTecherCalendarRepresentationType,
-  toggleRuleModalMode
+  toggleRuleModalMode,
+  subjectShowMore,
+  teacherShowMore
 } from "./navigation";
 
 
 const selectTeacherCalendarRepresentationType: TeacherSelector<TecherCalendarRepresentationType> = (state) => state.modal.techerCalendarRepresentationType;
 const selectRuleModalMode: TeacherSelector<ModalMode> = (state) => state.modal.ruleModalMode;
+const selectIsSubjectShowMore: TeacherSelector<boolean> = (state) => state.modal.subjectDescriptionShowMore;
+const selectIsTeacherShowMore: TeacherSelector<boolean> = (state) => state.modal.teacherDescriptionShowMore;
+
 
 
 describe('Teacher Novigation Commands', () => {
@@ -31,6 +36,30 @@ describe('Teacher Novigation Commands', () => {
     await teacherStore.asyncDispatch(toggleRuleModalMode.action());
     const changedState = teacherStore.getState();
     expect(selectRuleModalMode(changedState)).toBe(newRuleModalMode);
+  });
+
+  test('subject Decription ShowMore test', async () => {
+    const teacherStore = teacherStoreCreator();
+    const initialState = teacherStore.getState();
+    expect(selectIsSubjectShowMore(initialState)).toBeFalsy();
+    await teacherStore.asyncDispatch(subjectShowMore.action());
+    const changedState = teacherStore.getState();
+    expect(selectIsSubjectShowMore(changedState)).toBeTruthy();
+    await teacherStore.asyncDispatch(subjectShowMore.action());
+    const againChangedState = teacherStore.getState();
+    expect(selectIsSubjectShowMore(againChangedState)).toBeFalsy();
+  });
+
+  test('teacher Decription ShowMore test', async () => {
+    const teacherStore = teacherStoreCreator();
+    const initialState = teacherStore.getState();
+    expect(selectIsTeacherShowMore(initialState)).toBeFalsy();
+    await teacherStore.asyncDispatch(teacherShowMore.action());
+    const changedState = teacherStore.getState();
+    expect(selectIsTeacherShowMore(changedState)).toBeTruthy();
+    await teacherStore.asyncDispatch(teacherShowMore.action());
+    const againChangedState = teacherStore.getState();
+    expect(selectIsSubjectShowMore(againChangedState)).toBeFalsy();
   });
 });
 
