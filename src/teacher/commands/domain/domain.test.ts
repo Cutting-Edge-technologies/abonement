5c61ea5af30e2f5dbba022c27d5531867e9d6318
UrUrZa <IshvarIndrovich@mail.ru>
2022-11-24 06:47:02 +0400

    test start Editing and confirm saving Rule

--- src/teacher/commands/domain/domain.test.ts
+++ src/teacher/commands/domain/domain.test.ts
@@ -4,12 +4,17 @@ import {
   startEditingAbonementOffer,
   confirmSavingAbonement,
   startEditingRule,
-  startEditingSubject
+  startEditingSubject,
+  confirmSavingRule
 } from "./domain"
 import{
 changeAbonementLimitLessons,
 changeAbonementPrice,
-changeAbonementType
+changeAbonementType,
+changeRuleDuration,
+changeRulePeriodicityType,
+changeRuleMonthDay,
+changeRuleStartTime
   } from "../editable/editable"
 import {
   startCreatingAbonements,
@@ -22,12 +27,17 @@ selectTeacherAbonementOffers,
 selectAbonementOfferAbonementType,
 selectAbonementOfferLimitLessons,
 selectAbonementOfferPrice,
-selectRuleId
+selectRuleId,
+selectSubjectRules,
+selectRuleDuration,
+selectRulePeriodicityType,
+selectRuleStartTime,
+selectRuleperiodicityRule
 } from "../../selectors/editable"
 
 describe('Domain Commands', () => {
 
-  test('start Editing AbonemetOffer test', async () => {
+  test('start Editing and confirm saving AbonemetOffer test', async () => {
     const exampleLimitLessons = 15;
     const exampleAbonementPrice = 200;
     const exampleAbonementType = abonementType.limited;
@@ -51,23 +61,30 @@ describe('Domain Commands', () => {
     expect(selectAbonementOfferAbonementType(againChangedState)).toEqual(exampleAbonementType);
   });
 
-  //test('start Editing Rule test', async () => {
-  //  const teacherStore = teacherStoreCreator();
-  //  const initialState = teacherStore.getState();
-  //  expect(selectRuleId(initialState)).toBe('');
-  //  const ruleId = '123';
-  //  await teacherStore.asyncDispatch(startEditingRule.action(ruleId));
-  //  const changedState = teacherStore.getState();
-  //  expect(selectRuleId(changedState)).toBe(ruleId);
-  //});
-//
-  //test('start Editing Subject test', async () => {
-  //  const teacherStore = teacherStoreCreator();
-  //  const initialState = teacherStore.getState();
-  //  expect(selectSubjectId(initialState)).toBe('');
-  //  const subjectId = '123;'
-  //  await teacherStore.asyncDispatch(startEditingSubject.action(subjectId));
-  //  const changedState = teacherStore.getState();
-  //  expect(selectSubjectId(changedState)).toBe(subjectId);
-  //});
+  test('start Editing and confirm saving Rule test', async () => {
+    const exampleDurationMin = 50;
+    const exampleRulePeriodicityType = PeriodicityType.manualDate;
+    const exampleMonthDay = 8;
+    const exampleStartTime = 21;
+
+    const teacherStore = teacherStoreCreator();
+    const initialState = teacherStore.getState();
+    expect(selectSubjectRules(initialState)).toEqual([]);
+    await teacherStore.asyncDispatch(startCreatingRule.action());
+    await teacherStore.asyncDispatch(changeRuleDuration.action(exampleDurationMin));
+    await teacherStore.asyncDispatch(changeRulePeriodicityType.action(exampleRulePeriodicityType));
+    await teacherStore.asyncDispatch(changeRuleMonthDay.action(exampleMonthDay));
+    await teacherStore.asyncDispatch(changeRuleStartTime.action(exampleStartTime));
+    await teacherStore.asyncDispatch(confirmSavingRule.action());
+    const changedState = teacherStore.getState();
+    expect(selectSubjectRules(changedState).length).toBeGreaterThan(0);
+    const ruleId = selectTeacherAbonementOffers(changedState)[0];
+    await teacherStore.asyncDispatch(startEditingRule.action(ruleId));
+    const againChangedState = teacherStore.getState();
+    expect(selectRuleId(againChangedState)).toBe(ruleId);
+    expect(selectRuleDuration(againChangedState)).toEqual(exampleDurationMin);
+    expect(selectRulePeriodicityType(againChangedState)).toEqual(exampleRulePeriodicityType);
+    expect(selectRuleperiodicityRule(againChangedState)[0]).toEqual(exampleMonthDay);
+    expect(selectRuleStartTime(againChangedState)).toBe(exampleStartTime);
+  });
 });
