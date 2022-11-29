751fe6ff89dfe598cda892ee496cb93d3aaf856c
Kirill Penkin <kirill.penkin.doubleverify.com>
2022-11-29 18:42:59 +0400

    hoc tests

--- src/teacher/hoc/Example/Example.test.tsx
+++ src/teacher/hoc/Example/Example.test.tsx
@@ -1,44 +1,54 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 import { ExampleTeacher } from '.';
-import { HocDecorator } from '../TeacherStateDecorator';
+import { setTeacherState } from '../../../common/store/utils';
+import { exampleApiCommand, exampleResetCommand } from '../../commands/example/exampleCommands';
+import { createHOCDecorator } from '../TeacherStateDecorator';
 
 describe('Test Example HOC', () => {
   test('it checks that defaults are shown in HOC', async () => {
+    const { HocDecorator } = createHOCDecorator();
     const HocStoryToRender = HocDecorator(() => <ExampleTeacher />);
     const { container } = await render(HocStoryToRender);
+    
     const messageElement = container.querySelector('.message');
-    console.log(messageElement?.innerHTML);
+    const countElement = container.querySelector('.count');
 
-    const buttons = container.querySelectorAll('button');
-    const buttonSet = buttons[1];
-    const buttonsReset = buttons[2];
+    expect(messageElement?.innerHTML).toBe('The message is ');
+    expect(countElement?.innerHTML).toBe('The count is 0');
+  });
 
+  test('it checks that changes in the state are reflected in the components', async () => {
+    const { HocDecorator, teacherStore } = createHOCDecorator();
+    const HocStoryToRender = HocDecorator(() => <ExampleTeacher />);
+    const { container } = render(HocStoryToRender);
+    
     await act(async () => {
-      buttonSet.click();
-      await new Promise(r => setTimeout(r, 100));
-      console.log('After set');
-      console.log(messageElement?.innerHTML);
+      teacherStore.dispatch(setTeacherState({
+        example: {
+          message: 'Some generic message',
+          count: 99,
+        }
+      }));
     });
 
-    await act(async () => {
-      buttonsReset.click();
-      await new Promise(r => setTimeout(r, 100));
-      console.log('After reset');
-      console.log(messageElement?.innerHTML);
-    })
+    const messageElement = container.querySelector('.message');
+    const countElement = container.querySelector('.count');
 
-    expect(true).toBeTruthy();
-  })
-  test('it checks querring by role', async () => {
+    expect(messageElement?.innerHTML).toBe('The message is Some generic message');
+    expect(countElement?.innerHTML).toBe('The count is 99');
+  });
+
+  test('it checks that actions are dispatched on buttons click', () => {
+    const { HocDecorator, teacherStore } = createHOCDecorator();
     const HocStoryToRender = HocDecorator(() => <ExampleTeacher />);
-    render(HocStoryToRender);
-    const message = await screen.findByRole('message');
-    console.log(message);
-    console.log(message.innerHTML);
+    const { container } = render(HocStoryToRender);
 
-    // const button
+    const buttons = container.querySelectorAll('button');
+    buttons[1].click();
+    expect(teacherStore.getActionHistory()[0].type).toBe(exampleApiCommand.action.type);
 
-    expect(true).toBeTruthy();
-  })
+    buttons[2].click();
+    expect(teacherStore.getActionHistory()[1].type).toBe(exampleResetCommand.action.type);
+  });
 })
