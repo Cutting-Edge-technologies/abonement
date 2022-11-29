3281a62944752d383b554e3cba4969577a06807a
Kirill Penkin <kirill.penkin.doubleverify.com>
2022-11-29 19:07:56 +0400

    no need for this

--- src/teacher/hoc/TeacherStateDecorator/index.tsx
+++ src/teacher/hoc/TeacherStateDecorator/index.tsx
@@ -3,15 +3,6 @@ import { Provider } from "react-redux";
 import { makeHocTestingStore } from "../../../common/store/utils";
 import { teacherStoreCreator } from "../../store";
 
-export const HocDecorator = (Story: ComponentStory<any>) => {
-  const teacherStore = teacherStoreCreator();
-  return (
-    <Provider store={teacherStore}>
-      <Story />
-    </Provider>
-  )
-}
-
 export const createHOCDecorator = () => {
   const teacherStore = makeHocTestingStore(teacherStoreCreator());
   const HocDecorator = (Story: ComponentStory<any>) => {
